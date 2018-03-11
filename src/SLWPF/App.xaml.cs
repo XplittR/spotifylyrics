3c77ef444147859cd0e9050242bda967ec99fef3
Bendik August Nesbø <bendik.nesbo@gmail.com>
2018-03-11 13:38:37 +0100

    Added NLog for logging errors

--- src/SLWPF/App.xaml.cs
+++ src/SLWPF/App.xaml.cs
@@ -1,5 +1,42 @@
-﻿using System.Windows;
+﻿using System.Text;
+using System.Windows;
+using NLog;
+using NLog.Layouts;
 
 namespace SpotifyLyricsWPF {
-    public partial class App : Application { }
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+            SetupLogger();
+        }
+
+        protected override void OnExit(ExitEventArgs e) {
+            NLog.LogManager.Shutdown(); // Flush and close down internal threads and timer
+            base.OnExit(e);
+        }
+
+        private void SetupLogger() {
+            const string format = "${longdate} ${message} ${exception:format=tostring}";
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var logfile = new NLog.Targets.FileTarget() {
+                FileName = "SpotifyLyrics.log",
+                Name = "logfile",
+                Layout = new SimpleLayout(format),
+                Encoding = Encoding.UTF8,
+            };
+            config.LoggingRules.Add(new NLog.Config.LoggingRule("*", LogLevel.Debug, logfile));
+
+#if DEBUG
+            var logconsole = new NLog.Targets.ConsoleTarget() {
+                Name = "logconsole",
+                Layout = new SimpleLayout(format),
+                Encoding = Encoding.UTF8,
+            };
+            config.LoggingRules.Add(new NLog.Config.LoggingRule("*", LogLevel.Info, logconsole));
+#endif
+
+            NLog.LogManager.Configuration = config;
+        }
+    }
 }
--- src/SLWPF/MainWindow.xaml.cs
+++ src/SLWPF/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using SpotifyLyricsDomain;
 
 namespace SpotifyLyricsWPF {
     public partial class MainWindow : Window {
+        private static readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
         public BackendViewModel ViewModel { get; set; }
 
         public MainWindow() {
@@ -15,19 +16,21 @@ namespace SpotifyLyricsWPF {
             t.Tick += Tick;
             t.Interval = TimeSpan.FromSeconds(1);
             t.Start();
-            
-            
+
+
         }
 
         private void Tick(object sender, EventArgs e) {
             try {
                 ViewModel.UpdateTitle();
             } catch (Exception ex) {
+                Log.Error(ex, "Failed to update lyrics");
                 ViewModel.WindowTitle = "error";
             }
         }
 
         private void Options_OnClicked(object sender, RoutedEventArgs e) {
+            Log.Info("testInfo");
             //todo: stop the DispatcherTimer?
             var dlg = new OptionsDialog() { Owner = this };
             dlg.ShowDialog();
